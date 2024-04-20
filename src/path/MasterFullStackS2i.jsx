866962aafcca250465f8a32a14b2dad81ec9c931
boobaGreen <devboobagreen@gmail.com>
2024-04-20 23:23:48 +0200

    tenstack query

--- src/path/MasterFullStackS2i.jsx
+++ src/path/MasterFullStackS2i.jsx
@@ -72,7 +72,7 @@ function MasterFullStackS2i() {
                   TanStack Query
                 </div>
               </li>
-              s
+
               <li className="mr-1.5 mt-2">
                 <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
                   Node.js
