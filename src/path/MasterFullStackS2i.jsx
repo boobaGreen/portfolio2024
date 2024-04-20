5b67e7e73a15294771a89c18a00fa0af33907b7c
boobaGreen <devboobagreen@gmail.com>
2024-04-20 23:18:55 +0200

    styled components added

--- src/path/MasterFullStackS2i.jsx
+++ src/path/MasterFullStackS2i.jsx
@@ -94,6 +94,11 @@ function MasterFullStackS2i() {
               </li>
               <li className="mr-1.5 mt-2">
                 <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                  Styled Components
+                </div>
+              </li>
+              <li className="mr-1.5 mt-2">
+                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
                   Tailwind
                 </div>
               </li>
