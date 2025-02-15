35bb15cb37e4ae8e759582ae856659e5ac115816
boobaGreen <claudiodallara77@gmail.com>
2025-02-15 14:33:30 +0100

    no twitter temp

--- src/Header.jsx
+++ src/Header.jsx
@@ -28,7 +28,7 @@ function Header() {
               >
                 <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all duration-300 group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>{" "}
                 <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                  Certification
+                  Training
                 </span>
               </a>
             </li>
